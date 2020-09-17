08895ea080420a232c41b1a3538511adea09bff1
Hugo Chinchilla <hugoasecas@gmail.com>
2020-09-17 17:26:56 +0200

    A carrier can also have up to 4 bags

--- src/Carrier/Carrier.php
+++ src/Carrier/Carrier.php
@@ -5,18 +5,31 @@ declare(strict_types = 1);
 namespace Example\App\Carrier;
 
 use Example\App\Backpack;
+use Example\App\Bag;
 
 class Carrier
 {
     private Backpack $backpack;
+    private array $bags;
 
     public function __construct()
     {
         $this->backpack = new Backpack();
+        $this->bags = [];
     }
 
     public function backpack(): Backpack
     {
         return $this->backpack;
+    }
+
+    public function bags(): array
+    {
+        return $this->bags;
+    }
+
+    public function addBag(Bag $bag): void
+    {
+        $this->bags[] = $bag;
     }
 }
--- tests/CarrierTest.php
+++ tests/CarrierTest.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Example\Tests;
 
+use Example\App\Bag;
 use Example\App\Carrier\Carrier;
 use PHPStan\Testing\TestCase;
 
@@ -15,5 +16,29 @@ class CarrierTest extends TestCase
         $durance = new Carrier();
 
         $this->assertEmpty($durance->backpack()->items());
+    }
+
+    /** @test */
+    public function a_carrier_can_have_a_bag(): void
+    {
+        $durance = new Carrier();
+        $a_bag = new Bag();
+
+        $durance->addBag($a_bag);
+
+        $this->assertEquals([$a_bag], $durance->bags());
+    }
+
+    public function a_carrier_can_have_up_to_4_bags(): void
+    {
+        $durance = new Carrier();
+
+        $durance->addBag(new Bag());
+        $durance->addBag(new Bag());
+        $durance->addBag(new Bag());
+        $durance->addBag(new Bag());
+        $durance->addBag(new Bag());
+
+        $this->assertEquals(4, count($durance->bags()));
     }
 }
